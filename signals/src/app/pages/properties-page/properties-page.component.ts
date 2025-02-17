e916fd6143123de65ab12b45eda04d50e489eb59
David González <david.gonzalez@atm-maggioli.es>
2025-02-17 10:25:27 +0100

    Signals computed and effect signals

--- signals/src/app/pages/properties-page/properties-page.component.ts
+++ signals/src/app/pages/properties-page/properties-page.component.ts
@@ -1,15 +1,53 @@
-import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, signal, type OnInit } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-properties-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './properties-page.component.html',
   styleUrl: './properties-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PropertiesPageComponent implements OnInit {
 
+  public counter = signal(10)
+
+
+  public user = signal<User>({
+    id: 1,
+    first_name: 'George',
+    last_name: 'Bluth',
+    email: 'george.bluth@reqres.in',
+    avatar: "https://reqres.in/img/faces/1-image.jpg"
+  })
+
+  public userChangeEffect = effect(() => {
+    console.log(`Usuaio: ${this.user().first_name} Contador: ${this.counter()}`)
+
+  })
+
+  incrementar(value:number){
+    this.counter.update(current => current + value)
+  }
+
+
   ngOnInit(): void { }
+
+  onFieldUpdated(field : keyof User, value:string){
+
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value
+    // })
+
+    this.user.update( current => ({
+      ...current,
+      [field]: value
+    }))
+
+
+  }
 
 }
